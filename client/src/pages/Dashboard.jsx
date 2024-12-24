30e7b467918b4f2fdf2336b449ac0c3d6c75f729
emmanboika <emmanuel@n-vibe.fr>
2024-12-24 06:58:41 +0100

    Dashboard page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,32 +1,61 @@
-import React from "react";
-import { Box, Grid, Paper, Typography } from "@mui/material";
-import ProtectedRoute from "../router/ProtectedRoute";
+import React, { useEffect, useRef, useState } from "react";
+import { Box, Grid, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+import DashboardCard from "../components/Dashboard/DashboardCard";
+import { getCards } from "../components/Dashboard/cards";
+import useServerApi from "../hooks/useServerApi";
 
 const Dashboard = () => {
+  const {
+    id: userId,
+    role,
+    companyId,
+    firstName,
+  } = useSelector((store) => store.user || {});
+
+  const [stats, setStats] = useState([]);
+  const requestRef = useRef("allowed");
+
+  const [{ loading, error }, refresh] = useServerApi(
+    {
+      url: "/auth/stats",
+      method: "get",
+    },
+    { manual: true }
+  );
+
+  const getStats = async () => {
+    try {
+      const response = await refresh({ params: { userId, role, companyId } });
+      setStats(response.data);
+      requestRef.current = "allowed";
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    if (requestRef.current === "allowed") {
+      requestRef.current = "denied";
+      if (userId && role) {
+        getStats();
+      }
+    }
+  }, [userId, role, companyId]);
+  if (loading) return <Box>Chargement...</Box>;
+  if (error) return <Box>Erreur: {error.message}</Box>;
   return (
-    <Box>
-      <Typography variant='h4' gutterBottom>
-        Bienvenue sur la Dashboard
+    <Box sx={{ flexGrow: 1 }}>
+      <Typography variant='h5' gutterBottom>
+        Bienvenue sur la Dashboard, {firstName}
       </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <Paper sx={{ p: 2 }}>
-            <Typography variant='h6'>Employés</Typography>
-            <Typography variant='h4'>42</Typography>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Paper sx={{ p: 2 }}>
-            <Typography variant='h6'>Congés en attente</Typography>
-            <Typography variant='h4'>5</Typography>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Paper sx={{ p: 2 }}>
-            <Typography variant='h6'>Paiements</Typography>
-            <Typography variant='h4'>€ 15,000</Typography>
-          </Paper>
-        </Grid>
+      <Grid
+        container
+        padding={1}
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}>
+        {stats?.map((card, index) => (
+          <DashboardCard key={index} card={card} />
+        ))}
       </Grid>
     </Box>
   );
